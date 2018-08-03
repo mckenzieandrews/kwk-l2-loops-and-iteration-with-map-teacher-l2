2ab9facc3386d340ef520f4c1b0f6abd5bcb0ae8
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-08-03 14:56:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-// Code your solution in this file.
-let students = ['Adele', 'Beyoncé', 'Lady', 'Madonna', 'Rihanna', 'Taylor']
-
-
-function lowerCaseStudentNames(student){
-    
-  return list.map( function (driver){
+function lowerCaseDrivers (list) {
+  return list.map(function (driver) {
     return driver.toLowerCase();
-  })
-  
+  });
+}
+
+function nameToAttributes (list) {
+  return list.map(function (driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
 }
